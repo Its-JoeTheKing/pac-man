1e7afc249cfc2f5bde727369217fb6fed151febf
Aymen Errfig <aerrfig@e1r3p5.1337.ma>
2024-02-04 13:00:18 +0100

    road finder

--- path_tracing.c
+++ path_tracing.c
@@ -61,6 +61,8 @@ void	init_val(char **map, t_point *player, t_point *size, int val)
 		map[y - 1][x + 1] = val + 48;
 	if (x - 1 >= 0 && y + 1 < size->y && map[y + 1][x - 1] == '0')
 		map[y + 1][x - 1] = val + 48;
+	if (map[y][x] == 'E')
+		printf("%d\n", val - 1);
 }
 
 void	init_val_2(char **map, t_point *size, int val)
@@ -115,6 +117,68 @@ char	*ft_strdup(const char *s1)
 	str[i] = '\0';
 	return (str);
 }
+void	check_corners(char **map, t_point *pos, t_point size, int val)
+{
+	
+	if (pos->x + 1 < size.x && pos->y + 1 < size.y && map[pos->y + 1][pos->x + 1] == val)
+	{
+		pos->x++;
+		pos->y++;
+	}
+	if (pos->x - 1 >= 0 && pos->y - 1 >= 0 && map[pos->y - 1][pos->x - 1] == val)
+	{
+		pos->x--;
+		pos->y--;
+	}
+	if (pos->x + 1 < size.x && pos->y - 1 >= 0 && map[pos->y - 1][pos->x + 1] == val)
+	{
+		pos->x++;
+		pos->y--;
+	}
+	if (pos->x - 1 >= 0 && pos->y + 1 < size.y && map[pos->y + 1][pos->x - 1] == val)
+	{
+		pos->x--;
+		pos->y++;
+	}
+}
+t_point get_next_pos(char **map, t_point size, t_point pont, int val)
+{
+	t_point pos;
+
+	pos.y = pont.y;
+	pos.x = pont.x;
+	pos.x += (pos.x + 1 < size.x && map[pos.y][pos.x + 1] == val) * 1;
+	pos.x -= (pos.x - 1 >= 0 && map[pos.y][pos.x - 1] == val) * 1;
+	pos.y += (pos.y + 1 < size.y && map[pos.y + 1][pos.x] == val) * 1;
+	pos.y -= (pos.y - 1 >= 0 && map[pos.y - 1][pos.x] == val) * 1;
+	if (map[pos.y][pos.x] == val)
+		return (pos);
+	check_corners(map, &pos, size, val);
+	return (pos);
+}
+
+void	get_road(char **map, t_point size, t_point pos, int val)
+{
+	t_point	*road;
+	int		i;
+
+	i = 0;
+	road = (t_point *)malloc(sizeof(t_point) * (val + 1));
+	road[i] = pos;
+	while (val > 1)
+	{
+		road[i] = get_next_pos(map, size, road[i - 1], val - 1);
+		val--;
+		i++;
+	}
+	i = 0;
+	while (road[i].x)
+	{
+		printf("%d %d\n", road[i].x, road[i].y);
+		i++;
+	}
+}
+
 int	main(void)
 {
 	int	val;
@@ -139,5 +203,10 @@ int	main(void)
 	player.y = 3;
 	player.x = 1;
 	floodmap(map, &player, &size);
+	t_point enemy;
+	enemy.y = 1;
+	enemy.x = 6;
+	t_point poo =  get_next_pos(map, size, enemy, '7');
+	printf("%d %d\n", poo.x, poo.y);
 	aff_map(map, size);
 }
